affacf3e892f83d12d5fefcf2a2fffea2f5df74b
ruks <ruky68@live.com>
2014-12-03 12:57:53 +0530

    move from ruks to unhostedcse

--- firefox_run.sh
+++ firefox_run.sh
@@ -3,6 +3,6 @@ rm -f unhosted@unhosted.projects.uom.lk.xpi
 zip -r unhosted@unhosted.projects.uom.lk.xpi .
 zip -r ../unhosted@unhosted.projects.uom.lk.xpi .
 cd ../
-mv unhosted@unhosted.projects.uom.lk.xpi ~/.mozilla/firefox/z0f42hwa.default/extensions
+mv unhosted@unhosted.projects.uom.lk.xpi ~/.mozilla/firefox/cl3yr8ek.default-1415592935346/extensions
 pkill -9 firefox
 firefox
